3ade04e9dc3fcc45eaeaff7e135b22f20aef8067
eric nicolas <eric.e.nicolas@gmail.com>
2018-05-03 23:02:38 -0700

    added tests

--- WordCount.Tests/ModelTests/WordString.Test.cs
+++ WordCount.Tests/ModelTests/WordString.Test.cs
@@ -8,10 +8,34 @@ namespace wordcount.Tests
   [TestClass]
   public class wordtest
   {
+        [TestMethod]
+    public void testempty()
+    {
+      Wordstring test = new Wordstring ("", "hello");
+      int count = test.count();
+      int counter = 0;
+      Assert.AreEqual(counter, count);
+    }
     [TestMethod]
     public void testcount()
     {
       Wordstring test = new Wordstring ("hello hello hey", "hello");
+      int count = test.count();
+      int counter = 2;
+      Assert.AreEqual(counter, count);
+    }
+    [TestMethod]
+    public void testwithnumbers()
+    {
+      Wordstring test = new Wordstring ("hello3 hello hey", "hello");
+      int count = test.count();
+      int counter = 1;
+      Assert.AreEqual(counter, count);
+    }
+    [TestMethod]
+    public void testwithpunctuations()
+    {
+      Wordstring test = new Wordstring ("hello. hello! hey.", "hello");
       int count = test.count();
       int counter = 2;
       Assert.AreEqual(counter, count);
